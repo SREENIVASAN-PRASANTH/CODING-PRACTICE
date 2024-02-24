9ece4de40da2184fc447d2f985630ab3dd5f3120
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-02-24 23:32:21 +0530

    solved the max_contiguous_subarray problem using kadane's algorithm

--- python DSA/max_subarray.py
+++ python DSA/max_subarray.py
@@ -1,18 +1,18 @@
 List = list(map(int,input("Enter the Array: ").split()))
 
 def max_subarray(arr):
-    max_ending_here = max_so_far = arr[0]
+    current_max = max = arr[0]
     start = end = temp_start = 0
     
     for i in range(1,len(arr)):
-        if arr[i] > max_ending_here + arr[i]:
-            max_ending_here = arr[i]
+        if arr[i] > current_max + arr[i]:
+            current_max = arr[i]
             temp_start = i
         else:
-            max_ending_here += arr[i]
+            current_max += arr[i]
             
-        if max_ending_here > max_so_far:
-            max_so_far = max_ending_here
+        if current_max > max:
+            max = current_max
             start = temp_start
             end = i
     return arr[start:end + 1]
