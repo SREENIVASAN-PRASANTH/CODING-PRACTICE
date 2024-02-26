61475cbd7967a3418454cf9a11a878a9d02f580f
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-02-26 17:38:34 +0530

    palindrome program added

---
+++ java programs practice/palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class palindrome {
+    public static void main(String[] args) {
+      String original,reverse = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the element: ");
+        original = sc.nextLine();
+        int length = original.length();
+
+        for(int i = length - 1;i >=0 ; i--){
+            reverse = reverse + original.charAt(i);
+        }
+
+        if (original.equals(reverse)){
+            System.out.println("It is a palindrome");
+        }
+        else{
+            System.out.println("It is not a palindrome");
+        }
+    }
+}
