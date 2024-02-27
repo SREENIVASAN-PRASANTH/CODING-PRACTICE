4b56cbb26deb90485c4dc57393148e72ccb4bace
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-02-27 20:07:04 +0530

    matrix_rotations

---
+++ java programs practice/squarePattern.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class squarePattern{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows and columns: ");
+        String row_column = sc.nextLine();
+        
+    }
+}
