a8fac79acf7ed96972d8165b5703e6313e12889b
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-02-28 20:12:09 +0530

    added count variable in program

--- java programs practice/even_no_count.java
+++ java programs practice/even_no_count.java
@@ -6,10 +6,14 @@ public class even_no_count {
         System.out.print("Enter the range: ");
         int number = sc.nextInt();
         System.out.print("List of even numbers from 1 to " + number + ": ");
+        int count = 0;
         for (int i = 1;i<=number;i++){
             if(i % 2 ==0){
                 System.out.print(i + " ");
+                count ++;
             }
         }
+        System.out.println();
+        System.out.println("Total even numbers: " + count);
     }
 }
--- java programs practice/odd_no_count.java
+++ java programs practice/odd_no_count.java
@@ -5,10 +5,14 @@ public class odd_no_count {
         System.out.print("Enter the range: ");
         int number = sc.nextInt();
         System.out.print("List of odd numbers from 1 to " + number + ": ");
+        int count = 0;
         for (int i = 1;i<=number;i++){
             if(i % 2 !=0){
                 System.out.print(i + " ");
+                count ++;
             }
         }
+        System.out.println();
+        System.out.println("Total odd numbers : " + count);
     }
 }
