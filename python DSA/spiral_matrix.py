d3d2e25c6030dc741555761f5bebfb7b9a72c15f
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-05-05 21:05:37 +0530

    completed printing elements of a matrix in a spiral way

---
+++ python DSA/spiral_matrix.py
@@ -0,0 +1,47 @@
+#getting the matrix
+def get_matrix(row):
+     matrix = []
+     for i in range(row):
+         row_elements = list(map(int,input().split()))
+         matrix.append(row_elements)
+     return matrix
+
+if __name__ == "__main__":
+    row = int(input("Enter the row size: "))
+    matrix = get_matrix(row)
+    #assigning the top botom and left right values to access the elements in the matrix
+    top = 0
+    bottom = len(matrix)
+    left = 0
+    right = len(matrix[0])
+    
+    '''
+    consider a matrix
+     --     --
+     | 1 2 3 |
+     | 4 5 6 |
+     | 7 8 9 |
+     --     --
+     '''
+    while (top < bottom and left < right):
+        
+        for i in range(left,right):
+            print(matrix[top][i],end = "->") #It will iterate elements 1 -> 2 -> 3
+            
+        top = top + 1
+        
+        for i in range(top,bottom):
+            print(matrix[i][right - 1],end = "->") #It  will iterate the elements 6 -> 9
+            
+        right = right - 1
+        
+        for i in range(right-1,left - 1,-1):
+            print(matrix[bottom - 1][i], end = "->") #This will print 8 -> 7
+        
+        bottom = bottom - 1
+        
+        for i in range(bottom-1,top - 1,-1):
+            print(matrix[i][left], end = "->") #the last iteration 4 -> 5
+        
+        left = left + 1
+    print("None")
