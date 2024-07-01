252c636180de23a94432ad8061b4c1a8e3854953
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-07-01 18:56:13 +0530

    completed hoisted session concepts

--- javascirpt programs practice/hoisting_array_methods.js
+++ javascirpt programs practice/hoisting_array_methods.js
@@ -48,5 +48,49 @@ console.log(name);
 const numbers = [1,2,3,4,5];
 let square = numbers.map((number) => number * number);
 console.log(square);
-*/
 
+6)Using forEach()
+
+const arr = [1,2,3,4,5]
+let count = 0;
+arr.forEach((val) => {
+    count = count + val;
+    console.log(count);
+})
+
+7)Using reduce()
+
+const arr = [3, 4, 5, 6];
+let b = arr.reduce((accumulator, element) => accumulator + element);
+
+console.log(b)
+
+
+8)Using filter()
+
+const arr = [7,8,9,0]
+let result = arr.filter((number) => number <= 7);
+
+console.log(result);
+
+9)Using some() & every()
+
+let arr = ["a", "b", "c"];
+let result = arr.every((val) => val == "a");
+console.log(result);
+
+let result2 = arr.some((val) => val === "b");
+console.log(result2);
+
+10)Using reverse() to reverse values of a array
+
+let arr = [1,2,3,4,5];
+let res = arr.reverse();
+
+console.log(res);
+
+11)Using the flat()
+
+let arr2 = [1,2,[[[[[4]]]]]]
+console.log(arr2.flat(3));
+*/
