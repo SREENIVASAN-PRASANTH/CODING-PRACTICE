f1a9562c299ead5a81ca610dc1e55f50ff26d962
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-08-02 22:57:34 +0530

    creating an instance of a function

--- javascirpt programs practice/DAY4.js
+++ javascirpt programs practice/DAY4.js
@@ -8,3 +8,31 @@ console.log(Object.getPrototypeOf(myArray));
 let sportsArray = new Array("football", "VolleyBAll");
 
 console.log(Object.getPrototypeOf(sportsArray));
+
+// creating a function using constructor
+let printAge = new Function(
+  "name",
+  `this.name = name;
+    console.log(this.name);`
+);
+
+printAge("Prasanth");
+
+console.log(Object.getPrototypeOf(printAge));
+
+// creating An instance of a function
+function getMarks(subject,marks){
+    this.subject = subject;
+    this.marks = marks;
+    this.getMarksResult = function(){
+        console.log(`${this.subject} : ${this.marks}`)
+    }
+}
+
+let person1 = new getMarks("Physics",91);
+person1.getMarksResult();
+
+console.log(person1);
+
+console.log(getMarks.prototype);
+
