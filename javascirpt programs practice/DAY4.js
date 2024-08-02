bc0ce598543803456e010a8cb4772613bfc77695
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-08-02 23:15:31 +0530

    checking prototypes and getting instance of prototypes

--- javascirpt programs practice/DAY4.js
+++ javascirpt programs practice/DAY4.js
@@ -36,3 +36,36 @@ console.log(person1);
 
 console.log(getMarks.prototype);
 
+//adding properties and methods to prototype
+
+function books(name, price){
+    this.name = name;
+    this.price = price;
+}
+
+let book1 = new books("Python",230);
+let book2 = new books("Data Structures", 1000);
+
+console.log(book1);
+console.log(book2);
+
+books.prototype.calculateDiscount = function(discount){
+    return (discount/100) * this.price;
+}
+
+console.log(book1.calculateDiscount(20));
+console.log(book2.calculateDiscount(100));
+
+// checking prototypes of book1 and book2
+console.log(
+    Object.getPrototypeOf(book1) === Object.getPrototypeOf(book2)
+);
+
+// getting instance properties
+console.log(
+    Object.getOwnPropertyNames(book1)
+);
+
+console.log(
+    Object.getOwnPropertyNames(book2)
+);
